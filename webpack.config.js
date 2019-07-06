a1cdcdbb731dcacad61126803dc6876c03f50279
dnaganog <dnaganog@gmail.com>
2019-07-06 12:30:46 -0400

    updated babel preset-react

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,13 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {presets: ['@babel/preset-env']}
+        loader: 'babel-loader',
+        options: {presets: ['@babel/preset-env', '@babel/preset-react']}
       }
     ]
   },
   output: {
-    path: path.resolve(__dirname, "dist/"),
+    path: path.resolve(__dirname, 'dist/'),
     publicPath: '/dist/',
     filename: 'bundle.js'
   }
