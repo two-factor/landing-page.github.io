37638058fd0d8eb7cc8a7158dcfdd20ec58c155e
dnaganog <dnaganog@gmail.com>
2019-07-06 12:28:03 -0400

    add dev hot-loading

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,24 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './index.js',
-  mode: 'development',
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            options: {presets: ['@babel/preset-env']}
-          }
-        ]
-      },
-        output: {
-          path: path.resolve(__dirname, "dist/"),
-          publicPath: '/dist/',
-          filename: 'bundle.js'
-        }
+  mode: process.env.NODE_ENV,
+  devServer: {
+    publicPath: '/build',
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {presets: ['@babel/preset-env']}
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  }
 }
